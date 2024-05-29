43f3ce957ce967a98a6fa7c74b767ce29fd2723d
Gabriel Santiago <gabrielsnti2002@gmail.com>
2024-05-29 15:01:48 -0300

    Add: Documentação Classe Pedido

--- codigo/ComidinhasVeganas/spring/src/main/java/djavan/demo/models/Pedido.java
+++ codigo/ComidinhasVeganas/spring/src/main/java/djavan/demo/models/Pedido.java
@@ -4,8 +4,15 @@ import jakarta.persistence.ManyToOne;
 
 import jakarta.persistence.*;
 
+/**
+ * Entidade que representa um pedido no sistema.
+ */
 @Entity
 @Table(name = "pedido")
+
+/**
+ * Construtor padrão da classe Pedido.
+ */
 public class Pedido {
 
 	@Id
@@ -37,6 +44,16 @@ public class Pedido {
 	public Pedido() {
 	}
 
+	/**
+     * Construtor da classe Pedido.
+     *
+     * @param id Identificador único do pedido.
+     * @param cardapio Cardápio associado ao pedido.
+     * @param dataPedido Data em que o pedido foi realizado.
+     * @param horaPedido Hora em que o pedido foi realizado.
+     * @param cliente Cliente que realizou o pedido.
+     * @param managerId Identificador do gerente responsável pelo pedido.
+     */
 	public Pedido(Long id, String cardapio, String dataPedido, String horaPedido, Cliente cliente, String managerId) {
 		this.id = id;
 		this.dataPedido = dataPedido;
@@ -45,10 +62,22 @@ public class Pedido {
 		this.managerId = managerId;
 	}
 
+	
+	/**
+     * Retorna o identificador do pedido.
+     *
+     * @return O identificador único do pedido.
+     */
 	public Long getId() {
 		return this.id;
 	}
 
+	
+	/**
+     * Define o identificador do pedido.
+     *
+     * @param id O novo identificador do pedido.
+     */
 	public void setId(Long id) {
 		this.id = id;
 	}
@@ -57,39 +86,93 @@ public class Pedido {
 		this.cardapio = cardapio;
 	}
 
+	
+	/**
+     * Retorna o cardápio associado ao pedido.
+     *
+     * @return O cardápio do pedido.
+     */
 	public Cardapio getcardapio() {
 		return cardapio;
 	}
 
+	
+	/**
+     * Define o cardápio do pedido.
+     *
+     * @param cardapio O novo cardápio do pedido.
+     */
 	public void setcardapio(Cardapio cardapio) {
 		this.cardapio = cardapio;
 	}
 
+	
+	/**
+     * Retorna o valor total do pedido.
+     *
+     * @return O valor total calculado a partir do cardápio.
+     */
 	public double getValor() {
 		return cardapio.getValor();
 	}
 
+	
+	/**
+     * Retorna a hora do pedido.
+     *
+     * @return A hora em que o pedido foi realizado.
+     */
 	public Object getHoraPedido() {
 		return horaPedido;
 	}
 
+	
+	/**
+     * Define a hora do pedido.
+     *
+     * @param horaPedido A nova hora em que o pedido foi realizado.
+     */
 	public void setHoraPedido(Object horaPedido) {
 		this.horaPedido = (String) horaPedido;
 	}
 
+	
+	/**
+     * Retorna a data do pedido.
+     *
+     * @return A data em que o pedido foi realizado.
+     */
 	public Object getDataPedido() {
 		return this.dataPedido = (String) dataPedido;
 
 	}
 
+	
+	/**
+     * Define a data do pedido.
+     *
+     * @param dataPedido A nova data em que o pedido foi realizado.
+     */
 	public void setDataPedido(Object dataPedido) {
 		this.dataPedido = (String) dataPedido;
 	}
 
+	
+	/**
+     * Define o cliente do pedido.
+     *
+     * @param cliente O novo cliente do pedido.
+     */
 	public void setCliente(Cliente cliente2) {
 		this.cliente = cliente2;
 	}
 
+	
+	/**
+     * Retorna o cliente associado ao pedido.
+     *
+     * @return O cliente que realizou o pedido.
+     */
 	public Cliente getCliente() {
 		return this.cliente;
 	}
